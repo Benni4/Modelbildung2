4cc0a1026e42d8f2b02643681fe4af139699e177
sumkin89 <n.kabiolskij@me.com>
2015-04-28 10:06:36 +0200

    paar standard Methoden

--- src/edu/hm/sim/inseldorf/Server.java
+++ src/edu/hm/sim/inseldorf/Server.java
@@ -2,7 +2,24 @@ package edu.hm.sim.inseldorf;
 
 public class Server implements Runnable {
 	private Queue queue;
+	private boolean busy;
 	
+	public Queue getQueue() {
+		return queue;
+	}
+
+	public void setQueue(Queue queue) {
+		this.queue = queue;
+	}
+
+	public boolean isBusy() {
+		return busy;
+	}
+
+	public void setBusy(boolean busy) {
+		this.busy = busy;
+	}
+
 	public Server(Queue queue) {
 		this.queue = queue;
 	}
