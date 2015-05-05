9b7138d58e44109cd8f38b19d0a9f0fd2ed48f4a
Bernhard Bücherl <bernhard.buecherl@gmail.com>
2015-05-05 11:26:21 +0200

    improved test simulation for console out

--- src/edu/hm/sim/inseldorf/DataCollector.java
+++ src/edu/hm/sim/inseldorf/DataCollector.java
@@ -1,5 +1,8 @@
 package edu.hm.sim.inseldorf;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DataCollector {
@@ -30,13 +33,59 @@ public class DataCollector {
 			spawnTimes.add((double)data);
 		} else { // CLIENT
 			clients.add((Client) data);
-			// currently just print out
-			System.out.println("Client " + data.toString() + " in Queue: " + simulation.getQueue().size());
 			queueSizes.add(simulation.getQueue().size());
+			
+			if(simulation.debug) {
+				System.out.println("Client " + data.toString() + " in Queue: " + simulation.getQueue().size());
+			}
 		}		
 	}
 	
 	public synchronized ArrayList<Object> get(int type) {
 		return null;
+	}
+	
+	public synchronized void print(String path) throws IOException {
+		// files
+		File clientFile = new File(path.replace("/", File.separator) + File.separator + "clients.txt");
+		File spawnFile = new File(path.replace("/", File.separator) + File.separator + "spawn.txt");
+		File processFile = new File(path.replace("/", File.separator) + File.separator + "process.txt");
+		File queueFile = new File(path.replace("/", File.separator) + File.separator + "queue.txt");
+		
+		// client output
+		clientFile.createNewFile();
+		FileWriter writer = new FileWriter(clientFile);
+		for(Client c : clients) {
+			writer.write(c.toString() + "\n");
+		}
+		writer.flush();
+		writer.close();
+	
+		// spawn output
+		spawnFile.createNewFile();
+		writer = new FileWriter(spawnFile);
+		for(double s : spawnTimes) {
+			writer.write(s + "\n");
+		}
+		writer.flush();
+		writer.close();
+		
+		// process output
+		processFile.createNewFile();
+		writer = new FileWriter(processFile);
+		for(double p : processTimes) {
+			writer.write(p + "\n");
+		}
+		writer.flush();
+		writer.close();
+		
+		// queue output
+		queueFile.createNewFile();
+		writer = new FileWriter(queueFile);
+		for(int q : queueSizes) {
+			writer.write(q + "\n");
+		}
+		writer.flush();
+		writer.close();
 	}
 }
--- src/edu/hm/sim/inseldorf/TestSim.java
+++ src/edu/hm/sim/inseldorf/TestSim.java
@@ -1,8 +1,36 @@
 package edu.hm.sim.inseldorf;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestSim {
-	public static void main(String...args) {
-		Simulation sim = new Simulation(100, 1000, 100);
-		sim.start();
+	public static void main(String...args) throws IOException, InterruptedException {
+		Simulation sim = new Simulation(100, 100, 100, true);
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String r = null;
+
+		while(true) {
+			Thread.sleep(1);
+			System.out.print(" > ");
+			r = reader.readLine();
+			
+			switch(r) {
+			case "start":
+				sim.start();
+				break;
+			case "pause":
+				sim.pause();
+				break;
+			case "exit":
+				sim.getCollector().print("resources");
+				sim.reset();
+				break;
+			case "quit":
+				return;
+			}
+		}
+	
 	}
 }
